d270f3acde3478ae1379875363b6bbb92ac7193a
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-22 07:19:21 -0800

    Style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,53 @@ import React from "react";
 
 const Header = ({ showCart, setShowCart }) => {
     return (
-        <div>
-            <Link to="/shopping-cart">
-                <h1>Gymshark</h1>
+        <div 
+            className="flex flex-col bg-neutral-800 py-4 gap-2 
+                        lg:flex-row lg:justify-between lg:px-48"
+        >
+            <Link to="/shopping-cart"
+                className="flex justify-center"
+            >
+                <h1
+                    className="text-4xl font-bold text-cyan-600 text-center md:text-5xl lg:text-[4.0rem]"
+                >
+                    Gymshark
+                </h1>
             </Link>
-            <nav>
-                <ul>
-                    <Link to="/shopping-cart">
-                        <li>Home</li>
+            <nav
+                className="flex justify-center"
+            >
+                <ul
+                    className="flex gap-6 md:gap-16"
+                >
+                    <Link to="/shopping-cart"
+                        className="mt-auto mb-auto"
+                    >
+                        <li
+                            className="text-2xl md:text-3xl lg:text-4xl font-bold text-cyan-600"
+                        >
+                            Home
+                        </li>
                     </Link>
                     
-                    <Link to="/shopping-cart/shop">
-                        <li>Shop</li>
+                    <Link to="/shopping-cart/shop"
+                        className="mt-auto mb-auto"
+                    >
+                        <li
+                            className="text-2xl md:text-3xl lg:text-4xl font-bold text-cyan-600"
+                        >
+                            Shop
+                        </li>
                     </Link>
                     <li>
                         <button 
-                            className="cursor-pointer"
                             onClick={() => setShowCart(!showCart)}
+                            className="cursor-pointer p-4 bg-cyan-600 rounded-full relative"
                         >
-                            <FontAwesomeIcon icon={faShoppingCart} />
+                            <FontAwesomeIcon 
+                                icon={faShoppingCart} 
+                                className="text-xl md:text-3xl lg:text-4xl inset-0"
+                            />
                         </button>
                     </li>
                 </ul>
