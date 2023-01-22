0c79a4a93168fdaf7c058c9ca93d5a294ac71f19
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-22 09:35:27 -0800

    Add style and responsivity to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,32 @@ import React from "react";
 
 const Home = () => {
     return (
-      <div>
-        <div>
-          <p>2023 Fitness Collection</p>
-          <Link to="/shop">Shop Now</Link>
+      <div
+        className="relative flex justify-center items-center text-xl
+            md:mx-16 md:text-1xl
+            lg:mx-32 md:text-4xl"
+      >
+        <div
+          className="absolute my-auto mx-auto text-neutral-800 font-bold flex flex-col items-center justify-center
+                      bg-white w-[100%] h-[100%] bg-opacity-20 gap-2"
+        >
+          <p
+            className="text-neutral-900"
+          >
+            2023 Fitness Collection
+          </p>
+          <Link 
+            to="/shop"
+            className="bg-cyan-600 hover:bg-cyan-400 px-2 rounded-full
+                        md:px-4 py-2"
+          >Shop Now</Link>
         </div>
-        <img src={yogaClassImg} alt="People in yoga class"/>
+        <img 
+          src={yogaClassImg} 
+          alt="People in yoga class"
+          className=" -z-10"
+          />
+        
       </div>
     );
   }
