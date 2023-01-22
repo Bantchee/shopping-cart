2f5f6f2d57fb64dd7f65c31d7c261ade566a68e4
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-22 09:00:02 -0800

    Footer styling and responsivity

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,24 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div>
-            <p>Made By J. Steven Jarrett</p>
-            <a href="https://github.com/Bantchee" target={"_blank"} rel="noreferrer">
-                <FontAwesomeIcon icon={faGithub}/>
+        <div
+            className="flex flex-col gap-1 text-xl items-center bg-neutral-800 text-cyan-600 py-2
+                        md:flex-row md:text-2xl md:justify-center md:gap-2
+                        lg:text-3xl lg:gap-4"
+        >
+            <p
+                className="font-bold"
+            >Made By J. Steven Jarrett</p>
+            <a 
+                href="https://github.com/Bantchee" 
+                target={"_blank"} 
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon 
+                    icon={faGithub}
+                    size="2x"
+                    className="hover:text-cyan-400"
+                />
             </a>
         </div>
     )
